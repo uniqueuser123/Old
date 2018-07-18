9d3ac75f52c4e72ea1c41cd028afea62c8329aad
Training_B8C.07.19 <Training_B8C.07.19@BDC8-DX-4TFRX52.dir.svc.accenture.com>
2018-07-18 12:09:19 +0530

    okiessss

--- Project/src/test/java/Project1/Class.java
+++ Project/src/test/java/Project1/Class.java
@@ -1,15 +1,27 @@
+package Project1;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class Class {
+@Parameters("browser")	
 @Test
-public void newtest() {
+public void newtest(String value)
+{
+	WebDriver driver=null;
+	 if(value.equals("chrome"))	
+	 {
 	System.setProperty("webdriver.chrome.driver","C:\\Users\\a.d.lakshminarayana\\Desktop\\chromedriver_win32\\chromedriver.exe");
-	WebDriver driver=new ChromeDriver();
+	driver=new ChromeDriver();
+	 }
+	else if(value.equals("firefox")){
+		System.setProperty("webdriver.gecko.driver","C:\\Users\\training_b8c.07.19\\Downloads\\geckodriver-v0.21.0-win64\\geckodriver.exe");
+		driver=new FirefoxDriver();	
+	}
 	driver.get("https://github.com/login");
 	driver.findElement(By.id("login_field")).sendKeys("uniqueuser123");
 	driver.findElement(By.id("password")).sendKeys("Shivaji123$");
@@ -19,6 +31,6 @@ public void newtest() {
 	//Assert.assertEquals(title,"Sign in to GitHub · GitHub");
 	Assert.assertEquals(title, "GitHub");
 	System.out.println("validated");
-	System.out.println("validated");
+	System.out.println("okies");
 }
 }
